4a85268685e4958808ce7835909cb52b6c672feb
tzuhan <haa831101@gmail.com>
2023-09-01 13:46:58 -0400

    thh

--- hyperqb.sh
+++ hyperqb.sh
@@ -121,10 +121,10 @@ fi
 printf "NuSMV models and HyperLTL formula parsing..."
 # RUN PARSER on Docker
 echo "(using docker for parsing, parsing could become slower)"
-# TIME_PARSE=$(docker run --platform linux/amd64 -v ${PWD}:/mnt tzuhanmsu/hyperqube:latest /bin/bash -c "cd mnt/; TIMEFORMAT="%Rs"; time python3 ${ARBITRARY_PARSER} ${OUTFOLDER} ${MODELS[@]} ${FORMULA} ${P} ${QSFILE} ${FLAG}; ")
+TIME_PARSE=$(docker run --platform linux/amd64 -v ${PWD}:/mnt tzuhanmsu/hyperqube:latest /bin/bash -c "cd mnt/; TIMEFORMAT="%Rs"; time python3 ${ARBITRARY_PARSER} ${OUTFOLDER} ${MODELS[@]} ${FORMULA} ${P} ${QSFILE} ${FLAG}; ")
 
 # RUN PARSER locally, if the setup on your local machine is successful
-TIME_PARSE=$(time python3 ${ARBITRARY_PARSER} ${OUTFOLDER} ${MODELS[@]} ${FORMULA} ${P} ${QSFILE} ${FLAG})
+# TIME_PARSE=$(time python3 ${ARBITRARY_PARSER} ${OUTFOLDER} ${MODELS[@]} ${FORMULA} ${P} ${QSFILE} ${FLAG})
 
 
 # if any error happens in parsing, exit HyperQB
--- run_demos.sh
+++ run_demos.sh
@@ -2,7 +2,15 @@
 #####################################################################################################
 #  This script runs ALL experiments (0.1-11.1) we provided in Table 3 of our tool paper submission  #
 #####################################################################################################
-
+HyperQB=./hyperqb.sh
+PES='-pes' # default value
+OPT='-opt'
+HPES='-hpes'
+HOPT='-hopt'
+# SINGLE='-single'
+# MULTI='-multi'
+BUGHUNT="-bughunt" # default value
+FIND="-find"
 ### Requirements:
 ###   1. Before running, please make sure Docker is installed: (https://docs.docker.com/get-docker/)
 ###   2. Apple Chop Macs might need to use: --platform linux/amd64
@@ -10,7 +18,7 @@
 ### DEMOS, see README for detailed explination
 
 ### [demo 1: run bakery algorithm with symmetry property]
-# ./hyperqb.sh demo/bakery.smv demo/bakery.smv demo/symmetry.hq 10 -pes
+# ./hyperqb.sh demo/bakery.smv demo/bakery.smv demo/symmetry.hq 1 -pes
 ### [dem0 2: run SNARK algorithm with linearizability propoerty]
 # ./hyperqb.sh demo/SNARK_conc.smv demo/SNARK_seq.smv demo/lin.hq 18 -pes
 ### [demo 3: run simple_krip with simple_krip_formula]
@@ -18,4 +26,8 @@
 # ./hyperqb.sh demo/mini.smv demo/mini.smv demo/mini.smv demo/mini.smv  demo/mini_try.hq 10 -pes -find
 # ./hyperqb.sh demo/mini.smv demo/mini.smv demo/mini.smv demo/mini.hq 3 -pes
 
-./hyperqb.sh demo/mini.smv demo/mini.smv demo/mini.smv demo/mini.hq 3 -pes
+# ./hyperqb.sh demo/mini.smv demo/mini.smv demo/mini.smv demo/mini.hq 3 -pes
+
+
+${HyperQB} cases_bmc/tacas_snark/snark1_M1_concurrent.smv cases_bmc/tacas_snark/snark1_M2_sequential.smv  cases_bmc/tacas_snark/snark1_formula.hq 1 ${PES} ${FIND}
+# ${HyperQB} cases_bmc/tacas_snark/snark2_new_M1_concurrent.smv  cases_bmc/tacas_snark/snark2_new_M2_sequential.smv cases_bmc/tacas_snark/snark2_formula.hq 30 ${PES} ${FIND}
